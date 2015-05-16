ef6111f1236b5b4e5c056a706511a4530740e184
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2015-05-17 01:12:17 +0300

    big progress of xmonad config, added xmobar

---
+++ xmobar.hs
@@ -0,0 +1,45 @@
+-- .xmobarrc
+
+Config {
+	font = "-*-terminus-bold-r-*-*-12-*-*-*-*-*-*-*",
+	bgColor = "black",
+	fgColor = "lightgray",
+	position = TopW L 100,
+	commands = [
+		Run Cpu [
+			"--template", "<total>",
+			"-L", "10", -- %
+			"-H", "50", -- %
+			"--normal", "green",
+			"--high", "red"
+		] 10,
+		Run MultiCpu [
+			"--template", "<autototal>",
+			"--Low",      "10", -- units: %
+			"--High",     "50", -- units: %
+			"--normal",   "green",
+			"--high",     "red"
+		] 10,
+		Run DynNetwork [
+			"--template", "<rx>/<tx>",
+			"--Low",      "1000", -- units: kB/s
+			"--High",     "5000", -- units: kB/s
+			"--low",      "#CEFFAC",
+			"--normal",   "#FFFFCC",
+			"--high",     "#FFB6B0"
+		] 10,
+		Run Memory [
+			"-t", "<usedratio>%"
+		] 10,
+		Run Swap [
+			"-t", "<usedratio>%"
+		] 10,
+		Run Date "%a %b %_d %l:%M" "date" 10,
+		Run StdinReader
+	],
+	sepChar = "%",
+	alignSep = "}{",
+	template = "%StdinReader% }{ %cpu% / %multicpu% <fc=gray>|</fc> <fc=skyblue>%memory%</fc>/<fc=skyblue>%swap%</fc> <fc=gray>|</fc> %dynnetwork% <fc=gray>|</fc> <fc=#ee9a00>%date%</fc>"
+}
+
+-- vim: ft=haskell :
--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,67 @@
 import XMonad
+import XMonad.Util.Run (spawnPipe)
 
-main = xmonad defaultConfig
-	{
-		modMask = mod4Mask, -- mod4 instead of alt key
-		terminal = "terminator"
-	}
+import XMonad.Layout.Circle
+import XMonad.Layout.Grid
+import XMonad.Layout.Tabbed
+
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+
+import System.IO
+
+myTerm = "terminator"
+
+myWorkspaces :: [String]
+myWorkspaces =
+	["1:main"]
+	++ map show [2..6]
+	++ ["7:media","8:im","9:im"]
+
+myConfig = defaultConfig {
+	manageHook = manageDocks <+> manageHook defaultConfig,
+	layoutHook = avoidStruts $ myLayoutHook,
+	
+	modMask    = mod4Mask, -- mod4 instead of alt key
+	terminal   = myTerm,
+	workspaces = myWorkspaces
+}
+
+myTabTheme = defaultTheme {
+	activeColor         = "#3c5863",
+	activeBorderColor   = "#000000",
+	inactiveColor       = "#666666",
+	inactiveBorderColor = "#000000",
+	decoHeight          = 10
+}
+
+myLayoutHook =
+	tiled ||| Mirror tiled ||| Full ||| Grid ||| Circle
+	||| tabbed shrinkText myTabTheme
+		where
+			tiled = Tall nmaster delta ration
+			nmaster = 1 -- default master count
+			ration = 2/3 -- master proportion
+			delta = 5/100 -- percent of master resize
+
+main = do
+	xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar.hs"
+	xmonad $ myConfig {
+		logHook = dynamicLogWithPP $ defaultPP {
+			ppOutput = System.IO.hPutStrLn xmproc,
+			ppTitle = xmobarColor "#FFB6B0" "" . shorten 100 .wrap "  [ <fc=gray>" "</fc> ]  ",
+			ppCurrent = xmobarColor "green" "" . wrap "[" "]",
+			ppSep = "  ",
+			ppWsSep = " ",
+			--ppLayout = const ""
+			ppLayout  = (\ x -> case x of
+				"Tall"            -> "[:]"
+				"Mirror Tall"     -> "[=]"
+				"Full"            -> "[ ]"
+				"Grid"            -> "[+]"
+				"Circle"          -> "[o]"
+				"Tabbed Simplest" -> "[t]"
+				_                 ->   x   ),
+			ppHiddenNoWindows = showNamedWorkspaces
+		}
+	} where showNamedWorkspaces wsId = if any (`elem` wsId) ['a'..'z'] then pad wsId else ""
