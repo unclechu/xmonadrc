d95f7166dc978d7b2c70a9ded450e1c51e1c279a
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2016-05-12 15:34:33 +0300

    added locks to xmobar

--- xmobar.hs
+++ xmobar.hs
@@ -5,16 +5,16 @@ Config {
   bgColor = "#222",
   fgColor = "lightgray",
   position = Static { xpos = 0 , ypos = 0, width = 1919, height = 10 },
-  commands = [
-    Run Date "%A %d %B %H:%M" "date" 10,
-    Run Kbd [ ("us", "<fc=red>US</fc>")
-            , ("usmy", "<fc=red>US</fc><fc=#900000>*</fc>")
-            , ("ru", "<fc=green>RU</fc>")
-            , ("rumy", "<fc=green>RU</fc><fc=#009000>*</fc>")
-            ],
-    Run UnsafeStdinReader
-  ],
+  commands = [ Run Date "%A %d %B %H:%M" "date" 10
+             , Run Locks
+             , Run Kbd [ ("us", "<fc=red>US</fc>")
+                       , ("usmy", "<fc=red>US</fc><fc=#900000>*</fc>")
+                       , ("ru", "<fc=green>RU</fc>")
+                       , ("rumy", "<fc=green>RU</fc><fc=#009000>*</fc>")
+                       ]
+             , Run UnsafeStdinReader
+             ],
   sepChar = "%",
   alignSep = "}{",
-  template = "%UnsafeStdinReader% }{ %kbd% <fc=#666>/</fc> <fc=#999>%date%</fc>"
+  template = "%UnsafeStdinReader% }{ <fc=yellow>%locks%</fc> <fc=#666>/</fc> %kbd% <fc=#666>/</fc> <fc=#999>%date%</fc>"
 }
