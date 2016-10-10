91fa613ff43215164c0e4abd254db79456b25878
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2016-10-10 22:00:23 +0500

    xmobar.hs: removed xkb layout map 'origus' and 'origru'

--- xmobar.hs
+++ xmobar.hs
@@ -7,11 +7,9 @@ Config {
   position = Static { xpos = 0 , ypos = 0, width = 1919, height = 10 },
   commands = [ Run Date "%A %d %B %H:%M" "date" 10
              , Run Kbd [ ("us", "<fc=red>US</fc>")
-                       , ("mine(us)", "<fc=red>US</fc><fc=#900000>*</fc>")
-                       , ("mine(origus)", "<fc=red>US</fc><fc=#900000>~</fc>")
                        , ("ru", "<fc=green>RU</fc>")
+                       , ("mine(us)", "<fc=red>US</fc><fc=#900000>*</fc>")
                        , ("mine(ru)", "<fc=green>RU</fc><fc=#009000>*</fc>")
-                       , ("mine(origru)", "<fc=green>RU</fc><fc=#009000>~</fc>")
                        ]
              , Run UnsafeStdinReader
              , Run CommandReader "~/.xmonad/xmobar-cmd.sh" "cmd"
